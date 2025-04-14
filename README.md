364d062dbce8fd94c54eda01211f9cc45fc580e3
Tyler Liu <tyler4long@gmail.com>
2025-04-14 14:12:42 -0700

    First working version

---
+++ README.md
@@ -0,0 +1,1 @@
+# RingCentral User Group Demo
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,30 @@ const rc = new RingCentral({
 
 const main = async () => {
   await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
-  console.log(rc.token.access_token);
+
+  // list all the group members
+  const groupId = process.env.RINGCENTRAL_GROUP_ID!;
+  const r = await rc.get(
+    `/restapi/v1.0/account/~/user-group/${groupId}/members`,
+  );
+  console.log(r.data);
+
+  // add or remove members from user group
+  const r2 = await rc.post(`/restapi/v1.0/account/~/user-group/bulk-assign`, {
+    items: [
+      {
+        groupId,
+        addedExtensionIds: [
+          process.env.RINGCENTRAL_MEMBER_ID!,
+        ], // this cannot be omitted, even if empty
+        removedExtensionIds: [
+          // process.env.RINGCENTRAL_MEMBER_ID!,
+        ], // this cannot be omitted, even if empty
+      },
+    ],
+  });
+  console.log(r2.data);
+
   await rc.revoke();
 };
 main();
